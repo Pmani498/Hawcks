08f14b811594a31cb6d708d9ffe8b6eafc9d4a51
Admin <Admin@DESKTOP-SHFQC7C>
2022-07-21 23:11:22 +0530

    dhsj

---
+++ Nani/src/test/java/sa/DemoTest.java
@@ -0,0 +1,16 @@
+package sa;
+
+import org.testng.annotations.Test;
+@Test
+public class DemoTest {
+	public void demo() {
+		System.out.println("First jenkins project");
+		System.out.println("Second maven project");
+	}
+
+	@Test
+	public void democycle() {
+		System.out.println("First jenkins project");
+		
+}
+}
